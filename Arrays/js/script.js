ba41a77907e5ffe4a875c5f212afc7692490b6e2
dirty6613 <cmacchi@comcast.net>
2014-02-08 11:27:33 -0500

    Arrays after lesson

---
+++ Arrays/js/script.js
@@ -0,0 +1,11 @@
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //indexes 0,1,2,3
+
+(muppetNames[1]) = "Rizzo";
+(muppetNames[2]) = "Fozzy";
+console.log(muppetNames[2]);
+
+var num = 3;
+
+console.log(muppetNames[num]);
