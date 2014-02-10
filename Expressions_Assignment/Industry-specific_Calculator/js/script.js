1b2b02f9004746bc488977e986b75a86543ecdcd
dirty6613 <cmacchi@comcast.net>
2014-02-09 19:57:54 -0500

    added industry specific solution

--- Expressions_Assignment/Industry-specific_Calculator/js/script.js
+++ Expressions_Assignment/Industry-specific_Calculator/js/script.js
@@ -1,7 +1,7 @@
-//Type Calculator
+//Industry-specific Calculator
 
-var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width.");
-var height = prompt("Please enter the height.");
-var area = width * height;
-var result = "The area of your rectrangle is " + area + " sq feet.";
+var prizeMoney = prompt("I am calculating the amount of memberships needed annually for GridironGod.com to break even of money given away. \nPlease enter the amount of money given away.");
+var cost = prompt("Please enter the cost of memberships.");
+var membersNeeded = prizeMoney / cost;
+var result = "The amount of members needed to break even is " + membersNeeded + " paid members";
 alert(result);
