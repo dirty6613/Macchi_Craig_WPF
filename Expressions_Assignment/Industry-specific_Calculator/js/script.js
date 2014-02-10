701f45787014910b300f9a67ba5b7128ee45d385
dirty6613 <cmacchi@comcast.net>
2014-02-09 19:34:51 -0500

    added files for Industry specific calc

---
+++ Expressions_Assignment/Industry-specific_Calculator/js/script.js
@@ -0,0 +1,7 @@
+//Type Calculator
+
+var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height;
+var result = "The area of your rectrangle is " + area + " sq feet.";
+alert(result);
