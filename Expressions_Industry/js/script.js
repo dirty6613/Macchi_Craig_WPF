1bb195bb1ae700b613d08d083e9deb5565e24617
dirty6613 <cmacchi@comcast.net>
2014-02-09 20:49:42 -0500

    Updated goal of calculator

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,6 @@
-//Industry-specific Calculator
+//Industry-specific Calculator for calculating amount of memberships needed for website payout and how much money would be made or loss based on memberships.
 
-var prizeMoney = prompt("I am calculating the amount of memberships needed annually for GridironGod.com to break even of money given away. \nPlease enter the amount of money given away.");
+var prizeMoney = prompt("I am calculating the amount of memberships needed annually for GridironGod.com to break even of money given away and how much money would be gained or lossed based on memberships. \nPlease enter the amount of money given away.");
 var cost = prompt("Please enter the cost of memberships.");
 var membersNeeded = prizeMoney / cost;
 var result = "The amount of members needed to break even is " + membersNeeded + " paid members";
