2cd156045bd562a68e72e62d960126fbde0bef36
dirty6613 <cmacchi@comcast.net>
2014-02-14 01:12:28 -0500

    Stuff your face I - Added variables

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,1 +1,18 @@
-// JavaScript Document
+// Activity: Conditionals Worksheet
+// Craig Macchi
+// 2/14/14
+
+/*
+Label the section of code appropriately
+Write the givens as variables
+The result should be calculated using variables, not literal values when possible.
+Create code that will make decisions based on the value of the givens
+Print the result of the decision-making to the console.
+Use only code and techniques learned in this class.
+*/
+
+// Stuff your face I
+
+// Must weigh 250 lbs to enter the heavy-weight division
+var competitorsWeight = 180;
+var minWeight = 250;
