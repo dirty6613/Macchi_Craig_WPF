b9d261acee1c1f1ccc678610bd9bad33168bdbb6
dirty6613 <cmacchi@comcast.net>
2014-02-17 21:21:21 -0500

    Industry - added last name var

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,11 +6,13 @@
 //set variable for gender
 var gender = prompt("Is the person male or female.");
 //if statement to convert male to Mr.
-if (gender = "male"){
+if (gender === "male"){
 	gender = "Mr."
 }
 //if statement to convert female to Mrs.
-if (gender = "female"){
+if (gender === "female"){
 	gender = "Mrs."
 }
+//set variable for buyer's last name
+var lastName = prompt("Please enter person's last name");
 prompt("Hello, " + gender + lastName + "!");
