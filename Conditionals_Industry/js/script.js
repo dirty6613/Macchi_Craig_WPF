f3d7c99b65595ee91182c4eb89b9d97e585d7b0e
dirty6613 <cmacchi@comcast.net>
2014-02-17 21:18:01 -0500

    Inudstry - added calculator + gender var

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,16 @@
 // Craig Macchi
 // 2/14/2014
 //Assignment: Conditional Industry
+
+//A telemarketing sales prompt based on potential buyer's responses
+//set variable for gender
+var gender = prompt("Is the person male or female.");
+//if statement to convert male to Mr.
+if (gender = "male"){
+	gender = "Mr."
+}
+//if statement to convert female to Mrs.
+if (gender = "female"){
+	gender = "Mrs."
+}
+prompt("Hello, " + gender + lastName + "!");
