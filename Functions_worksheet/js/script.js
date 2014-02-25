cbd666d4da4820daad321f9ac1c99e1e62ce5ff9
dirty6613 <cmacchi@comcast.net>
2014-02-24 20:44:32 -0500

    added some comments

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,9 +8,13 @@ var x = circleCircum(5);
 
 // Calculate the Circumference of a Circle
 function circleCircum(r){
+	//var for calc
 	var d = r * 2;
 	var n = 3.14;
+	//calc of circle circum
 	var circle = n * d;
+	//return value of circle
 	return circle;
 }
+//output to log
 console.log("The circumference of the circle is " + x + ".");
