c9a2572e450144c8d859c54fcd7a49d27df48a9b
dirty6613 <cmacchi@comcast.net>
2014-02-24 19:36:19 -0500

    added circus - established variables

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,3 +2,15 @@
 	2/24/2014
 	Functions Worksheet */
 
+// Circumference
+
+// Calculate the Circumference of a Circle
+//var of radius
+var radius = 5;
+//var for diameter is radius * 2
+var diameter = radius * 2;
+//n = pi or 3.14
+var n = 3.14;
+//circum calc is pi * diameter
+var circum = n * diameter;
+
