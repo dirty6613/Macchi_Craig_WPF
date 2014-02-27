0c5fe9b74b11a0046382d1cafd41ac20c2221443
dirty6613 <cmacchi@comcast.net>
2014-02-27 18:37:18 -0500

    added calc to function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,11 +3,14 @@
 	Functions Industry */
 	
 //DOT Compliance Evaluator
-//I manage drivers and it is against the law for them to work over 60 hours.  We must document their hours, mileage, and fuel.  This will assist with that process.
+//I manage drivers and it is against the law for them to work over 60 hours in a 7 day week.  We must document their hours, mileage, and fuel.  This will assist with that process.
 
 
 //function for driver input
 function driverLog(hours, mileage, fuel){
-
+	hours += hours;
+	mileage += mileage;
+	fuel += fuel;
 }
+
 	
