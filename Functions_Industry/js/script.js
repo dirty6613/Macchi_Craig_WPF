07dfdc8233254397d91e897e5952eeb73a88160f
dirty6613 <cmacchi@comcast.net>
2014-02-27 18:33:07 -0500

    Updated Industry for Trucking Log - added function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,13 @@
 /*	Craig Macchi
-	2/  /2014
+	2/27/2014
 	Functions Industry */
+	
+//DOT Compliance Evaluator
+//I manage drivers and it is against the law for them to work over 60 hours.  We must document their hours, mileage, and fuel.  This will assist with that process.
+
+
+//function for driver input
+function driverLog(hours, mileage, fuel){
+
+}
+	
