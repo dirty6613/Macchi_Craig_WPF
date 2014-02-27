6ba553b211b0b9fa8cce74d22e3179ec4878c021
dirty6613 <cmacchi@comcast.net>
2014-02-27 18:47:22 -0500

    Added prompts for input

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,14 +3,22 @@
 	Functions Industry */
 	
 //DOT Compliance Evaluator
-//I manage drivers and it is against the law for them to work over 60 hours in a 7 day week.  We must document their hours, mileage, and fuel.  This will assist with that process.
+//I manage drivers and it is against the law for them to work over 60 hours in a 7 day week.  We must document their hours, for each day.  This will assist with that process.
 
 
 //function for driver input
-function driverLog(hours, mileage, fuel){
-	hours += hours;
-	mileage += mileage;
-	fuel += fuel;
+function driverLog(Sun, Mon, Tues, Wed, Thurs, Fri, Sat){
+
 }
 
-	
+alert("It is against the law for drivers to work over 60 hours in a 7 day week.  We must document their hours, for each day.  This will assist with that process to prevent a driver from breaking the law.  \nPlease enter how many hours you worked in the previous 6 days.");
+
+var Sun = prompt("How many hours worked on Sunday?");
+var Mon = prompt("How many hours worked on Monday?");
+var Tues = prompt("How many hours worked on Tuesday?");
+var Wed = prompt("How many hours worked on Wednesday?");
+var Thurs = prompt("How many hours worked on Thursday?");
+var Fri = prompt("How many hours worked on Friday?");
+var Sat = prompt("How many hours worked on Saturday?");
+
+
