9d1a85679ca03e0e16ee81cff4e836cc5be738c5
dirty6613 <cmacchi@comcast.net>
2014-02-27 16:33:33 -0500

    established functions for 1,2,3 point system + variables

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,18 +1,22 @@
 /*	Craig Macchi
 	2/27/2014
-	Functions Personal */
-	
+	Functions Personal */	
 //A basketball score tally calculator
-//function for team A
-function teamA(){
-		
+//establish global variable for score
+var score;
+//function for 1 point
+function fg1(){
+	score =+ 1;
+}
+//function for 2 pointer
+function fg2(){
+	score =+ 2;	
+}
+//function for 3 pointer
+function fg3(){
+	score =+ 3;
 }
 
-function teamB(){
-	
-}
-
-
-while (point !== "end"){
-var point = prompt("Input amount of points just scored.  Type \"end\" to end game.  The score is " + teamA + "to " + teamB + ".");
+while (score !== "end"){
+var score = prompt("Input amount of points just scored.  Type \"end\" to end game.  The score is " + teamA + "to " + teamB + ".");
 }
