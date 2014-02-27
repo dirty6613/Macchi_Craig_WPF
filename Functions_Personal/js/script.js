0398f637320a345def330bf94a7d5ccf0c4be9f7
dirty6613 <cmacchi@comcast.net>
2014-02-27 16:28:18 -0500

    Added bball calc for personal.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,11 @@
 /*	Craig Macchi
-	2/  /2014
+	2/27/2014
 	Functions Personal */
+	
+//A basketball score tally calculator
+
+
+
+while (point !== "end"){
+var point = prompt("Input amount of points just scored.  Type \"end\" to end game.  The score is " + teamA + "to " + teamB + ".");
+}
