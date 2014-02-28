ff5b5f9044729bc372cd3c102374c921167e6cac
dirty6613 <cmacchi@comcast.net>
2014-02-27 21:51:00 -0500

    added wacky start - sleep calculator

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,10 @@
 /*	Craig Macchi
-	2/  /2014
+	2/27/2014
 	Functions Wacky */
+
+//This is a sleep calculator, it calculates the amount of time you sleep.
+//start app with description
+alert("This is a sleep calculator.  Besides the time your wasting doing this calculation, would you like to know how many hours you waste sleeping?");
+
+//
+
