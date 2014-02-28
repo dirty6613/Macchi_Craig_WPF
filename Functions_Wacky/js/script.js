d64c1fd70b6e3bf9a15204ed8ad591d07197e125
dirty6613 <cmacchi@comcast.net>
2014-02-27 21:53:29 -0500

    added function for daily sleep

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,5 +6,11 @@
 //start app with description
 alert("This is a sleep calculator.  Besides the time your wasting doing this calculation, would you like to know how many hours you waste sleeping?");
 
-//
+//function with hours of sleep for each day
+function sleepDaily(am, pm){
+	//hours added for the day
+	hoursDaily = am + pm;
+	//return to function
+	return hoursDaily;	
+}
 
